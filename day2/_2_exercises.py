2b1202121634ffb0a696d9609a67925f08a6acff
Christo <christo@devatstation.games>
2023-04-19 18:01:38 -0400

    About to start day 3

--- day2/_2_exercises.py
+++ day2/_2_exercises.py
@@ -1,12 +1,6 @@
-# with open("sample.txt") as file:
-#     text = file.read()
-# words = text.split()
-# for word in words:
-#     index = word.find("el")
-    # if index != -1:
-    #     print(index)
-        # print(word)
-
+text = "We got money in this town after some weird events, we want to put some of that money into sending stones and protection from divination, and since you have better access to shops in the city, can let us know if any shop iun the city has access to that?"
+words = text.split()
+print(len(words))
 
 # current_year = 2023
 # born_year = input("Which year you were born? ")
@@ -26,19 +20,19 @@
 
 
 
-from datetime import datetime
+# from datetime import datetime
 
-# We get the seconds of now to as precise as possible
-now_seconds = datetime.now().timestamp()
+# # We get the seconds of now to as precise as possible
+# now_seconds = datetime.now().timestamp()
 
-#we ask the date
-birthday = input("Birthday in YYYY-MM-DD? ")
+# #we ask the date
+# birthday = input("Birthday in YYYY-MM-DD? ")
 
-# We get the seconds of the user date to as precise as possible
-birthday_seconds = datetime.strptime(birthday, "%Y-%m-%d").timestamp()
+# # We get the seconds of the user date to as precise as possible
+# birthday_seconds = datetime.strptime(birthday, "%Y-%m-%d").timestamp()
 
-# we create a new date from the result of subtracting the two variables with  the seconds on them
-print(datetime.fromtimestamp(now_seconds - birthday_seconds))
+# # we create a new date from the result of subtracting the two variables with  the seconds on them
+# print(datetime.fromtimestamp(now_seconds - birthday_seconds))
 
 
 
