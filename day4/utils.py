01b93a390fa27bf5d341794ea1303acdb8bcf549
Christo <christo@devatstation.games>
2023-04-27 18:42:04 -0400

    Preparing for day 4

--- day4/utils.py
+++ day4/utils.py
@@ -1,12 +1,1 @@
-def guess_the_number(number):
-    guessed_number = input("Guess a number: ")
-    guessed_number = int(guessed_number)
-    if guessed_number == number:
-        print("CORRECT")
-        return True 
-    elif guessed_number > number:
-        print("Guess lower")
-    else:
-        print("Guess higher")
 
-    return False
