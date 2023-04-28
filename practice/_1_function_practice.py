bb30518f0cab8adc953057cf579a9f82b2931115
Nob_Goblin <thegamingvlogger@gmail.com>
2023-04-27 23:23:59 -0400

    a comment on a change which is very important

--- practice/_1_function_practice.py
+++ practice/_1_function_practice.py
@@ -2,3 +2,4 @@
 
 # Exercise 1.2: code a function named divide_and_conquer that has 3 integer parameters, the first parameter needs to get divided by the 2nd one and the result needs to be added to the 3rd one. then print the final result.
 
+#modify_it_in_some_way_lol
